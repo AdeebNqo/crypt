e36e17ad8718ae4fe91bd2379e70a4d43538cdfb
AdeebNqo <adeebnqo@gmail.com>
2013-04-22 22:15:48 +0200

    finished functor for Vignere cipher

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include "cipher.hpp"
 #include<string>
 #include<fstream>
+#include "functor.hpp"
 int main(int args, char** argv){
 	using namespace mhlzol004;
-	std::string key("1");
-	crypt<Ceaser, bool, bool> c(key);
-	std::ofstream output("decode.txt");
-	c.decode(std::cin, output);
+	//std::string key("1");
+	//crypt<Ceaser, bool, bool> c(key);
+	//std::ofstream output("decode.txt");
+	//c.decode(std::cin, output);
+	confuse k;
+	std::cout << k('T','E') << std::endl;
 }
